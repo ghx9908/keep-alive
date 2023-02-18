894c3f97b08635709b828c4c844412c5aaf4df4c
“hongxiang.gao” <hongxiang.gao@retech.work>
2023-02-18 21:45:23 +0800

    feat:keepalive 的使用

--- src/App.js
+++ src/App.js
@@ -1,4 +1,35 @@
+import { Route, Link, Routes } from "react-router-dom"
+import { KeepAliveProvider, withKeepAlive } from "keepalive-react-component"
+import Home from "./components/Home"
+import UserList from "./components/UserList"
+import UserAdd from "./components/UserAdd"
+let KeepAliveHome = withKeepAlive(Home, { cacheId: "Home" })
+let KeepAliveUserList = withKeepAlive(UserList, {
+  cacheId: "UserList",
+  scroll: true,
+})
+let KeepAliveUserAdd = withKeepAlive(UserAdd, { cacheId: "UserAdd" })
 const App = () => {
-  return <>App</>
+  return (
+    <KeepAliveProvider>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/list">用户列表</Link>
+        </li>
+        <li>
+          <Link to="/add">添加用户</Link>
+        </li>
+      </ul>
+
+      <Routes>
+        <Route path="/" element={<KeepAliveHome />}></Route>
+        <Route path="/list" element={<KeepAliveUserList />}></Route>
+        <Route path="/add" element={<KeepAliveUserAdd />}></Route>
+      </Routes>
+    </KeepAliveProvider>
+  )
 }
 export default App
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+const Home = (props) => {
+  return (
+    <div>
+      <button>重置UserAdd</button>
+      <button>重置UserList</button>
+    </div>
+  )
+}
+export default Home
